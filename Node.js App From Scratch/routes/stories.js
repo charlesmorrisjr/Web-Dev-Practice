dcda79cc15c776955b056f0e68f0a2717e4d2652
Charles Morris Jr <cmorrisjr777@gmail.com>
2023-07-06 13:31:20 -0400

    Added Single Story page

--- Node.js App From Scratch/routes/stories.js
+++ Node.js App From Scratch/routes/stories.js
@@ -23,6 +23,28 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 });
 
+// @desc    Show single story
+// @route   GET /stories/:id
+
+router.get('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id)
+      .populate('user')
+      .lean();
+    
+    if (!story) {
+      res.render('error/404');
+    }
+
+    res.render('stories/show', {
+      story
+    });
+  } catch (err) {
+    console.error(err);
+    res.render('error/404');
+  }
+});
+
 // @desc    Show add page
 // @route   GET /stories/add
 
