1add0780fbe11cb5e16bbc44aaa569351a238b20
Charles Morris Jr <cmorrisjr777@gmail.com>
2023-07-17 01:31:26 -0400

    Added session persistence

--- wds_learn_react_one_project/src/App.jsx
+++ wds_learn_react_one_project/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NewTodoForm } from "./NewTodoForm";
 import { TodoList } from "./TodoList";
 import "./styles.css";
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    return localValue === null ? [] : JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(title) {
     setTodos((currentTodos) => {
